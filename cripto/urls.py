8c61433f82725ff9d3d05d167f8d958af40bade1
daniel martins <daniel@p001300-work.(none)>
2011-11-25 11:50:24 -0400

    Criptografia de cesar com interface html funcional

--- cripto/urls.py
+++ cripto/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cesftec.views.home', name='home'),
-    url(r'cesar/', 'cesftec.cripto.views.cesar', name='cesar'),
+    url(r'cesar/$', 'cesftec.cripto.views.cesar', name='cesar'),
+    url(r'cesar/decrypt/$', 'cesftec.cripto.views.cesar_uncrypt', name='cesar_decrypt'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- cripto/views.py
+++ cripto/views.py
@@ -6,21 +6,21 @@ import string
 
 class CesarEncrypt(object):
     
-    def __init__(self, msg, key=3):
+    def __init__(self, key=3):
         self.alfa = list(string.ascii_lowercase)
-        self.msg = msg
+        self.alfa.append(' ') # inserindo espaço na lista de caracteres
         self.key = key
         
     def separar(self, msg):
         # Guarda as chaves para as letras das mensagems
         # Exemplo: A = 0, B = 1 e etc.
         chavesMsg = []
-        for m in msg:
+        for m in msg.lower():
             chavesMsg.append(self.alfa.index(m))
         return chavesMsg
             
-    def encriptografar(self):
-        self.chavesMsg = self.separar(self.msg)
+    def encriptografar(self, msg):
+        self.chavesMsg = self.separar(msg)
         self.msgCriptografada = ""
         for chave in self.chavesMsg:
             idx_novo_valor = chave + self.key
@@ -30,8 +30,8 @@ class CesarEncrypt(object):
         return self.msgCriptografada
             
     
-    def descriptografar(self):
-        self.chavesMsgCriptografada = self.separar(self.msgCriptografada)
+    def descriptografar(self, msgCriptografada):
+        self.chavesMsgCriptografada = self.separar(msgCriptografada)
         self.msgDescriptografada = ""
         for chave in self.chavesMsgCriptografada:
             idx_valor = chave - self.key
@@ -45,11 +45,26 @@ def cesar(request):
     """
     Implementação da cifra de cesar
     """
-    msg2encrypt = "teste"
-    crypto = CesarEncrypt(msg2encrypt, 3)
-    form = CesarForm()
+    viewData = {}
+    formCesar = CesarForm()
+    if request.method == 'POST':
+        formCesar = CesarForm(request.POST)
+        if formCesar.is_valid():
+            crypto = CesarEncrypt(int(formCesar.cleaned_data['chave']))
+            request.session['cesar_msg_criptografada'] = viewData['msg_criptografada'] = crypto.encriptografar(formCesar.cleaned_data['mensagem'])
+            request.session['cesar_key'] = int(formCesar.cleaned_data['chave'])
+            
+    viewData['cesar_form'] = formCesar
     #return HttpResponse(crypto.encriptografar() + "<br/>" + crypto.descriptografar())
-    return render_to_response('base.html', {"cesar_form": form}, context_instance=RequestContext(request))
-
-
-
+    return render_to_response('base.html', viewData,
+                              context_instance=RequestContext(request))
+    
+def cesar_uncrypt(request):
+    if request.method == 'GET':
+        if request.session.get('cesar_msg_criptografada', '') <> '':
+            crypto = CesarEncrypt(request.session['cesar_key'])
+            msgDescript = crypto.descriptografar(request.session.get('cesar_msg_criptografada', ''))
+            return HttpResponse(msgDescript)
+    return HttpResponse('')
+    
+    
